28a3f8942edec9b75df7dff035e2d01826512594
Leman5 <leman5kirme5@gmail.com>
2021-04-27 22:44:13 +0530

    backend started but not tested

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class StationDetails(models.Model):
+    station_username = models.CharField(max_length=100,primary_key=True)
+    password = models.CharField(max_length=255,default=None)
+    email = models.EmailField(max_length=200,unique=True)
+    fname = models.CharField(max_length=255,default=None,null=True)
+    lname = models.CharField(max_length=255,null=True)
+    city = models.CharField(max_length=50,null=True)
+    state = models.CharField(max_length=50,null=True)
+    pincode = models.CharField(max_length=10,null=True)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from passlib.hash import pbkdf2_sha256,bcrypt
+from main.models import StationDetails
 
 # Create your views here.
 
@@ -25,16 +27,29 @@ def Log(request):
 
 def adminlogcredentials(request):
     if request.POST['id'] == 'admin' and request.POST['pass'] == 'admin123' :
-        return render(request,'Add-station.html')
+        return render(request,'Add station.html')
 
 def Stationlist(request):
-        return render(request,'Station-list.html')
+        return render(request,'Station list.html')
 
 def adminlogout(request):
         return render(request,'logout.html')
 
 def Addtrains(request):
-        return render(request,'Add-trains.html')
+        return render(request,'Add trains.html')
 
 def Addstation(request):
-        return render(request,'Add-station.html')
+        return render(request,'Add station.html')
+
+def AddStationDetails(request):
+    s = StationDetails()
+    s.station_username = request.POST['username']
+    s.password = request.POST['password']
+    s.email = request.POST['email']
+    s.fname = request.POST['fname']
+    s.lname = request.POST['lname']
+    s.city = request.POST['city']
+    s.state = request.POST['state']
+    s.pincode = request.POST['pincode']
+    s.save()
+    return HttpResponse('')
